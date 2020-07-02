dcd73aae5740531fabbdf601fd2907e77a40144f
Goutam <goutam.de158@gmail.com>
2020-07-02 13:26:10 +0530

    initial services implemented.

---
+++ user-account-service/src/main/java/com/aws/bms/useraccountservice/UserAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aws.bms.useraccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAccountServiceApplication.class, args);
+	}
+
+}
